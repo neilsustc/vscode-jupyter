267d0d95808608889e818aa09e983d850ab4017e
Yu <583181285@qq.com>
2018-11-03 23:11:18 +0000

    🎨

--- browser/src/components/Header/index.tsx
+++ browser/src/components/Header/index.tsx
@@ -1,36 +1,45 @@
 import * as React from 'react';
 
 interface HeaderProps {
-  clearResults: () => any;
-  toggleAppendResults: () => any;
-  appendResults: boolean;
+    clearResults: () => any;
+    toggleAppendResults: () => any;
+    appendResults: boolean;
 }
 
 interface HeaderState {
-  /* empty */
+    /* empty */
 }
 
 class Header extends React.Component<HeaderProps, HeaderState> {
 
-  constructor(props?: HeaderProps, context?: any) {
-    super(props, context);
-  }
+    constructor(props?: HeaderProps, context?: any) {
+        super(props, context);
+    }
 
-  render() {
-    return (
-      <header style={{ position: 'fixed', top: '0', left: '0', width: 'calc(100% - 1em)', padding: '.5em', boxShadow: '0px 0px 6px rgba(0, 0, 0, 0.5)' }}>
-        <label>
-          <input type="checkbox"
-            style={{ verticalAlign: 'middle' }}
-            checked={this.props.appendResults}
-            onChange={() => this.props.toggleAppendResults()} />
-          Append Results
-        </label>
-        &nbsp;
-        <button onClick={() => this.props.clearResults()}>Clear Results</button>
-      </header>
-    );
-  }
+    private styles: React.CSSProperties = {
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: 'calc(100% - 1em)',
+        padding: '.5em',
+        boxShadow: '0px 0px 6px rgba(0, 0, 0, 0.5)'
+    };
+
+    render() {
+        return (
+            <header style={this.styles}>
+                <label>
+                    <input type="checkbox"
+                        style={{ verticalAlign: 'middle' }}
+                        checked={this.props.appendResults}
+                        onChange={() => this.props.toggleAppendResults()} />
+                    Append Results
+                </label>
+                &nbsp;
+                <button onClick={() => this.props.clearResults()}>Clear Results</button>
+            </header>
+        );
+    }
 }
 
 export default Header;
--- browser/src/components/ResultList/index.tsx
+++ browser/src/components/ResultList/index.tsx
@@ -1,36 +1,40 @@
 import * as React from 'react';
-import Result from '../Result'
+import Result from '../Result';
+
 interface ResultProps {
-  results: NotebookResultsState;
+    results: NotebookResultsState;
 }
 
 interface ResultState {
-  /* empty */
+    /* empty */
 }
 
 class ResultList extends React.Component<ResultProps, ResultState> {
-  private renderResult(result: NotebookOutput) {
-    return
-  }
-  render() {
-    let results = this.props.results.map(result =>
-      <Result key={result.id} result={result} />
-    );
-    return (
-      <div id={'results-list'}
-        style={{
-          position: 'fixed',
-          top: 'calc(.5em + 21px + .5em + .5em + .3em)', // top-pos + btn-height + bottom-margin + top-margin + top-padding
-          left: '.5em',
-          padding: '0 1em 0 .5em',
-          width: 'calc(100% - .5em - .5em - 1em)', // 100% - left-pos - left-padding - right-padding
-          height: 'calc(100% - 1.8em - 21px)',
-          overflow: 'auto'
-        }}>
-        {results}
-      </div>
-    );
-  }
+
+    private renderResult(result: NotebookOutput) {
+        return
+    }
+
+    private styles: React.CSSProperties = {
+        position: 'fixed',
+        top: 'calc(.5em + 21px + .5em + .5em + .3em)',
+        left: '.5em',
+        padding: '0 1em 0 .5em',
+        width: 'calc(100% - .5em - .5em - 1em)',
+        height: 'calc(100% - 1.8em - 21px)',
+        overflow: 'auto'
+    };
+
+    render() {
+        let results = this.props.results.map(result =>
+            <Result key={result.id} result={result} />
+        );
+        return (
+            <div id={'results-list'} style={this.styles}>
+                {results}
+            </div>
+        );
+    }
 }
 
 export default ResultList;
